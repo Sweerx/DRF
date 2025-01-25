18f0b3981517a82f67d8b1a32ffe4e3b251944ce
Dmitry Yakushin <yakushin.dima8@gmail.com>
2025-01-25 13:28:19 +0300

    задания 1, 2, 3 готово

--- materials/models.py
+++ materials/models.py
@@ -6,6 +6,8 @@ class Course(models.Model):
     name = models.CharField(max_length=150, verbose_name="Название")
     preview = models.ImageField(upload_to="materials/courses/previews/", blank=True, null=True)
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, verbose_name="Владелец", null=True,
+                              blank=True, related_name="courses")
 
     class Meta:
         verbose_name = "Курс"
@@ -20,8 +22,10 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to="materials/courses/previews/", blank=True, null=True)
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
     video_link = models.TextField(verbose_name="Ссылка на видео", blank=True, null=True)
-    course = models.ForeignKey(Course, on_delete=models.PROTECT, verbose_name="Курс", null=True, blank=True,
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс", null=True, blank=True,
                                related_name="lessons")
+    owner = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, verbose_name="Владелец", null=True,
+                              blank=True, related_name="lessons")
 
     class Meta:
         verbose_name = "Урок"
