a8204601fcbaef3df5478dbce53a590d5717caac
Dmitry Yakushin <yakushin.dima8@gmail.com>
2025-02-09 15:57:47 +0300

    Полный рефакторинг

--- users/views.py
+++ users/views.py
@@ -1,16 +1,22 @@
+from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from materials.models import Payment
-from src.utils import get_queryset_for_owner, check_session_status, create_stripe_price, create_stripe_session
+from materials.serializers import PaymentSerializer
+from src.utils import check_session_status, create_stripe_price, create_stripe_session, get_queryset_for_owner
+
 from .models import User
-from .serializers import PaymentSerializer, UserSerializer, NewUserSerializer, UserDetailSerializer
-from rest_framework.permissions import AllowAny, IsAdminUser
 from .permissions import IsCurrentUser, IsModerator, IsOwner
+from .serializers import NewUserSerializer, UserDetailSerializer, UserSerializer
 
 
-
+# Create your views here.
 class UserListCreateAPIView(generics.ListCreateAPIView):
     """
     Дженерик для отображения списка и создания нового объекта User:
@@ -97,3 +103,48 @@ class PaymentListCreateAPIView(generics.ListCreateAPIView):
         payment.session_id = session_id
         payment.link = payment_link
         payment.save()
+
+
+class PaymentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Дженерик для просмотра, редактирования и удаления объекта Payment:
+    """
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def get_permissions(self):
+        """
+        Выдача разрешений в зависимости от статуса пользователя
+        """
+        if self.request.method == "GET":
+            self.permission_classes = [IsOwner | IsModerator | IsAdminUser]
+        elif self.request.method in ["PATCH", "PUT", "DELETE"]:
+            self.permission_classes = [IsModerator | IsAdminUser]
+        return super().get_permissions()
+
+    def get_object(self):
+        """
+        Уточнение статуса для неоплаченного платежа при обращении к объекту
+        """
+        payment = super().get_object()
+        if payment.session_id and payment.status == "unpaid":
+            payment.status = check_session_status(payment.session_id)
+            payment.save()
+        return payment
+
+
+class MyToken(TokenObtainPairView):
+    """
+    Представление для получения токенов авторизации
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        """
+        Заполнение поля last_login при получении токенов авторизации
+        """
+        result = super().post(request, *args, **kwargs)
+        user = User.objects.get(email=request.data["email"])
+        user.last_login = timezone.now()
+        user.save()
+        return result
