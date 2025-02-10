a5fca992f0f376051cbf2ca1121f1ab505a4cf99
Dmitry Yakushin <yakushin.dima8@gmail.com>
2025-02-10 20:01:05 +0300

    Исправил

--- users/views.py
+++ users/views.py
@@ -10,7 +10,6 @@ from rest_framework.permissions import AllowAny, IsAdminUser
 from .permissions import IsCurrentUser, IsModerator, IsOwner
 
 
-
 class UserListCreateAPIView(generics.ListCreateAPIView):
     """
     Дженерик для отображения списка и создания нового объекта User:
@@ -97,3 +96,31 @@ class PaymentListCreateAPIView(generics.ListCreateAPIView):
         payment.session_id = session_id
         payment.link = payment_link
         payment.save()
+
+
+class PaymentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Дженерик для просмотра, редактирования и удаления объекта Payment:
+    """
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def get_permissions(self):
+        """
+        Выдача разрешений в зависимости от статуса пользователя
+        """
+        if self.request.method == "GET":
+            self.permission_classes = [IsOwner | IsModerator | IsAdminUser]
+        elif self.request.method in ["PATCH", "PUT", "DELETE"]:
+            self.permission_classes = [IsModerator | IsAdminUser]
+        return super().get_permissions()
+
+    def get_object(self):
+        """
+        Уточнение статуса для неоплаченного платежа при обращении к объекту
+        """
+        payment = super().get_object()
+        if payment.session_id and payment.status == "unpaid":
+            payment.status = check_session_status(payment.session_id)
+            payment.save()
+        return payment
