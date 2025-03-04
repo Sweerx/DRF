a283219e7145c5c53ef8a28d9e10438f04be1c3f
Dmitry Yakushin <yakushin.dima8@gmail.com>
2025-03-05 01:11:29 +0300

    Исправил

--- conf/settings.py
+++ conf/settings.py
@@ -11,7 +11,7 @@ load_dotenv(override=True)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = env.str("SECRET_KEY", "fallback-secret")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
 
 DEBUG = True
 
