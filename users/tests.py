3bdb4dd156cc813f515980d1bc2b07881f563173
Dmitry Yakushin <yakushin.dima8@gmail.com>
2025-03-05 01:13:23 +0300

    Исправил

--- users/tests.py
+++ users/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from mypedia.models import Course, Lesson, Payment
+from materials.models import Course, Lesson, Payment
 
 from .models import User
 
